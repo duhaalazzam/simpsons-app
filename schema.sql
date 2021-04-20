34de4309b09973d081f16a404970a8af2664c271
duhaalazzam <dohabalazzam@gmail.com>
2021-04-20 12:45:29 +0300

    save

--- schema.sql
+++ schema.sql
@@ -2,6 +2,6 @@ CREATE TABLE IF NOT EXISTS myTable(
     id SERIAL NOT NULL,
     quote VARCHAR(250) NOT NULL,
     characterName VARCHAR(250) NOT NULL,
-    img TEXT NO NULL,
+    img TEXT NOT NULL,
     charDir VARCHAR(250) NOT NULL
 );
